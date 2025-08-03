abc15c399cf3737c7bc5981dd9f7ddef7ac33467
Morisio <umusijcuk6@gmail.com>
2025-08-04 00:23:08 +0300

    Added a check for text in the input field.

--- script.js
+++ script.js
@@ -5,25 +5,35 @@ button.addEventListener("click", () => addTaskToList(inputTask.value));
 
 function addTaskToList(taskText) {
    if (taskText === "") {
+      alert("Напиши що хочеш зробити.");
+   } else {
+      inputTask.value = "";
+
+      const listOfTasks = document.querySelector(".todo-list__list");
+
+      const task = document.createElement("div");
+      const checkbox = document.createElement("input");
+      const text = document.createElement("p");
+      const button = document.createElement("button");
+
+      checkbox.onclick = () => {
+         if (checkbox.checked) {
+            text.style.textDecoration = "line-through";
+         } else {
+            text.style.textDecoration = "none";
+         }
+      }
+
+      checkbox.type = "checkbox";
+      checkbox.name = "task";
+      text.textContent = taskText;
+      button.textContent = "видалити";
       
+      task.classList.add("todo-list__task");
+      listOfTasks.appendChild(task);
+      task.append(checkbox, text, button);
+
+
    }
-   
-   inputTask.value = "";
-
-   const listOfTasks = document.querySelector(".todo-list__list");
-
-   const task = document.createElement("div");
-   const checkbox = document.createElement("input");
-   const text = document.createElement("p");
-   const button = document.createElement("button");
-
-   checkbox.type = "checkbox";
-   checkbox.name = "task";
-   text.textContent = taskText;
-   button.textContent = "видалити";
-   
-   task.classList.add("todo-list__task");
-   listOfTasks.appendChild(task);
-   task.append(checkbox, text, button);
 }
 
