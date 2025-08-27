daa73ba6a8466186d862b24a49999c8a2a023823
Morisio <umusijcuk6@gmail.com>
2025-08-27 20:01:10 +0300

    Новий початок над проєктом

--- script.js
+++ script.js
@@ -1,52 +1,36 @@
 const inputTask = document.querySelector(".todo-list__input");
 const button = document.querySelector(".todo-list__button");
-const listOfTasks = document.querySelector(".todo-list__list");
-
-button.addEventListener("click", () => addTaskToList(inputTask.value));
-
-function deleteTask(event) {
-   event.target.parentElement.remove();
-   saveTask();
-}
+const listOfTask = document.querySelector(".todo-list__list");
 
 function saveTask() {
-   localStorage.setItem("task", listOfTasks.innerHTML);
+   localStorage.setItem("task", listOfTask.innerHTML);
 }
 
 function loadTask() {
-   listOfTasks.innerHTML = localStorage.getItem("task");
+   listOfTask.innerHTML = localStorage.getItem("task");
+   saveTask();
 }
 
-function addTaskToList(taskText) {
-   if (taskText === "") {
+button.addEventListener("click", function () {
+   if (inputTask.value === "") {
       alert("Напиши що хочеш зробити.");
    } else {
-      inputTask.value = "";
-
       const task = document.createElement("div");
       const checkbox = document.createElement("input");
       const text = document.createElement("p");
       const button = document.createElement("button");
 
-      button.addEventListener("click", deleteTask);
-      checkbox.onclick = () => {
-         if (checkbox.checked) {
-            text.style.textDecoration = "line-through";
-         } else {
-            text.style.textDecoration = "none";
-         }
-      }
-
       checkbox.type = "checkbox";
       checkbox.name = "task";
-      text.textContent = taskText;
+      text.textContent = inputTask.value;
       button.textContent = "видалити";
       
       task.classList.add("todo-list__task");
-      listOfTasks.appendChild(task);
+      listOfTask.appendChild(task);
       task.append(checkbox, text, button);
-
-      saveTask();
    }
-}
+   inputTask.value = "";
+   saveTask();
+});
+
 loadTask();
